dbdb2298508257950bec02be9b3d1d9f789d8551
RodneyWiewior <59317041+RodneyWiewior@users.noreply.github.com>
2019-12-28 21:03:43 +0100

    Add files via upload

---
+++ rekurencyjniesilnia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace silniarekurencyjnie
+{
+
+    class Program
+    {
+        private static int silnia1(int i)
+        {
+            if (i < 1)
+                return 1;
+            else
+                return i * silnia1(i - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            string line;
+            int n;
+
+            Console.WriteLine("podaj dla jakiej liczby obliczyc silnie.\n");
+            line = Console.ReadLine();
+            n = int.Parse(line);
+
+            Console.WriteLine(n + "! = " + silnia1(n));
+
+
+        }
+    }
+}
